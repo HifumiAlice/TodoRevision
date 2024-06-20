c8b75271fe478672190ade7beaa47df74d7b2f46
Alice <hifumialice@gmail.com>
2024-06-20 19:11:33 +0900

    first setting

---
+++ src/main/kotlin/com/teamsparta/todorevision/TodoRevisionApplication.kt
@@ -0,0 +1,11 @@
+package com.teamsparta.todorevision
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodoRevisionApplication
+
+fun main(args: Array<String>) {
+    runApplication<TodoRevisionApplication>(*args)
+}
