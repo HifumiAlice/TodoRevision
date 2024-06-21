243bb557e901dbaa73d67c16f656ffbb8cffe7ec
Alice <hifumialice@gmail.com>
2024-06-21 15:52:06 +0900

    :sparkles: feat: 예외처리 핸들러 생성

--- src/main/kotlin/com/teamsparta/todorevision/infra/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/teamsparta/todorevision/infra/exception/GlobalExceptionHandler.kt
@@ -1,4 +1,15 @@
 package com.teamsparta.todorevision.infra.exception
 
+import com.teamsparta.todorevision.infra.exception.dto.ErrorResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
 class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException::class)
+    fun handleIllegalArgumentException(e: IllegalArgumentException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity.badRequest().body(ErrorResponse(e.message!!))
+    }
 }
--- src/main/kotlin/com/teamsparta/todorevision/infra/exception/dto/ErrorResponse.kt
+++ src/main/kotlin/com/teamsparta/todorevision/infra/exception/dto/ErrorResponse.kt
@@ -1,2 +1,5 @@
-package com.teamsparta.todorevision.infra.exception.dto 
+package com.teamsparta.todorevision.infra.exception.dto
 
+data class ErrorResponse (
+    val message: String
+)
