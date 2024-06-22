18780a2a6401d9af914dd498a98d878c36c7e996
Alice <hifumialice@gmail.com>
2024-06-22 22:01:01 +0900

    댓글 레포 생성

--- src/main/kotlin/com/teamsparta/todorevision/domain/comment/repository/CommentRepository.kt
+++ src/main/kotlin/com/teamsparta/todorevision/domain/comment/repository/CommentRepository.kt
@@ -1,2 +1,9 @@
-package com.teamsparta.todorevision.domain.comment.repository 
+package com.teamsparta.todorevision.domain.comment.repository
 
+import com.teamsparta.todorevision.domain.comment.model.Comment
+import org.springframework.data.jpa.repository.JpaRepository
+
+
+interface CommentRepository: JpaRepository<Comment, Long> {
+
+}
