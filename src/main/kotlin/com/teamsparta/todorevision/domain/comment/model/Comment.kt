a3a4bdb2c31c5d8586268dbcc449e53007dac066
Alice <hifumialice@gmail.com>
2024-06-22 22:01:44 +0900

    내용 수정 함수 수정 및 response변환 함수 추가

--- src/main/kotlin/com/teamsparta/todorevision/domain/comment/model/Comment.kt
+++ src/main/kotlin/com/teamsparta/todorevision/domain/comment/model/Comment.kt
@@ -1,5 +1,8 @@
 package com.teamsparta.todorevision.domain.comment.model
 
+import com.fasterxml.jackson.annotation.JsonFormat
+import com.teamsparta.todorevision.domain.comment.dto.response.CommentResponse
+import com.teamsparta.todorevision.domain.member.dto.response.MemberResponse
 import com.teamsparta.todorevision.domain.member.model.Member
 import jakarta.persistence.*
 import java.time.LocalDateTime
@@ -9,28 +12,37 @@ import java.time.LocalDateTime
 class Comment(
 
     @Column(name = "content", nullable = false)
-    private var content : String,
-
-    @Column(name = "create_at", nullable = false)
-    private var createdAt : LocalDateTime,
+    private var content: String,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
     private var member: Member
 
 
-    ) {
+) {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private var id : Long? = null
+    private var id: Long? = null
 
-    fun getContent() : String = content
-    fun getCreatedAt() : LocalDateTime = createdAt
-    fun getMember() : Member = member
-    fun getId() : Long? = id
+    @Column(name = "create_at", nullable = false)
+    private var createdAt: LocalDateTime = LocalDateTime.now()
 
-    fun updateComment(comment: Comment) {
-        this.content = comment.content
+    fun getContent(): String = content
+    fun getCreatedAt(): LocalDateTime = createdAt
+    fun getMember(): Member = member
+    fun getId(): Long? = id
+
+    fun updateContent(content: String) {
+        this.content = content
+    }
+
+    fun toResponse(): CommentResponse {
+        return CommentResponse(
+            commentId = id!!,
+            content = content,
+            createdAt = createdAt,
+            member = member.toResponse()
+        )
     }
 
 
