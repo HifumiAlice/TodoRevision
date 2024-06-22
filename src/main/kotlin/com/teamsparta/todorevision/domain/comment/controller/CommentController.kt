e4015205d96f8c33c54f886763bcf7eced9e593e
Alice <hifumialice@gmail.com>
2024-06-22 22:02:06 +0900

    댓글 cud 초안 구현

--- src/main/kotlin/com/teamsparta/todorevision/domain/comment/controller/CommentController.kt
+++ src/main/kotlin/com/teamsparta/todorevision/domain/comment/controller/CommentController.kt
@@ -3,28 +3,46 @@ package com.teamsparta.todorevision.domain.comment.controller
 import com.teamsparta.todorevision.domain.comment.dto.request.CommentCreateRequest
 import com.teamsparta.todorevision.domain.comment.dto.request.CommentUpdateRequest
 import com.teamsparta.todorevision.domain.comment.dto.response.CommentResponse
+import com.teamsparta.todorevision.domain.comment.service.CommentService
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RequestMapping("/comments")
 @RestController
-class CommentController {
+class CommentController(
+    private val commentService: CommentService
+) {
 
     @PostMapping("/{todoId}")
     fun createComment(
         @PathVariable todoId: Long,
+        @RequestParam(name = "memberId") memberId: Long,
         @RequestBody request: CommentCreateRequest
     ): ResponseEntity<CommentResponse> {
-        TODO()
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(commentService.createComment(todoId, request, memberId))
     }
 
     @PutMapping("/{commentId}")
-    fun updateComment(@PathVariable commentId: Long, @RequestBody request : CommentUpdateRequest) : ResponseEntity<CommentResponse> {
-        TODO()
+    fun updateComment(
+        @PathVariable commentId: Long,
+        @RequestParam(name = "memberId") memberId: Long,
+        @RequestBody request: CommentUpdateRequest
+    ): ResponseEntity<CommentResponse> {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(commentService.updateComment(commentId, request, memberId))
     }
 
     @DeleteMapping("/{commentId}")
-    fun deleteComment(@PathVariable commentId: Long): ResponseEntity<Unit> {
-        TODO()
+    fun deleteComment(
+        @PathVariable commentId: Long,
+        @RequestParam(name = "memberId") memberId: Long
+    ): ResponseEntity<Unit> {
+        return ResponseEntity
+            .status(HttpStatus.NO_CONTENT)
+            .body(commentService.deleteComment(commentId, memberId))
     }
 }
