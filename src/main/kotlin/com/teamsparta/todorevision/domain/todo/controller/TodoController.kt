be4f86eec9a701e02d83f4af716b8786e679f128
Alice <hifumialice@gmail.com>
2024-06-24 17:48:54 +0900

    aop로 유저 정보 받아오기

--- src/main/kotlin/com/teamsparta/todorevision/domain/todo/controller/TodoController.kt
+++ src/main/kotlin/com/teamsparta/todorevision/domain/todo/controller/TodoController.kt
@@ -4,6 +4,10 @@ import com.teamsparta.todorevision.domain.todo.dto.request.TodoCreateRequest
 import com.teamsparta.todorevision.domain.todo.dto.request.TodoUpdateRequest
 import com.teamsparta.todorevision.domain.todo.dto.response.TodoResponse
 import com.teamsparta.todorevision.domain.todo.service.TodoService
+import com.teamsparta.todorevision.infra.aop.MemberDetails
+import com.teamsparta.todorevision.infra.aop.MemberPrincipal
+import io.swagger.v3.oas.annotations.Parameter
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -11,17 +15,21 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("/todos")
 class TodoController(
-    private val todoService: TodoService
+    private val todoService: TodoService,
 ) {
 
+    @MemberPrincipal("USER")
     @PostMapping()
     fun createTodo(
         @RequestBody request: TodoCreateRequest,
-        @RequestParam(name = "memberId") memberId: Long
+        @RequestHeader headers: HttpHeaders,
+        @Parameter(hidden = true) @ModelAttribute memberDetails : MemberDetails
     ): ResponseEntity<TodoResponse> {
-        // TODO: 나중에 memberId 제외하고 헤더에서 jwt를 통해서 값 받기
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(todoService.createTodo(request, memberId))
+        if (memberDetails.id == null) {
+            throw IllegalArgumentException("인증/인가 안됨")
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(todoService.createTodo(request, memberDetails.id!!))
     }
 
     @GetMapping()
@@ -34,37 +42,52 @@ class TodoController(
         return ResponseEntity.status(HttpStatus.OK).body(todoService.getTodoById(todoId))
     }
 
+    @MemberPrincipal("USER")
     @PutMapping("/{id}")
     fun updateTodo(
         @PathVariable(value = "id") todoId: Long,
-        request: TodoUpdateRequest,
-        @RequestParam(name = "memberId") memberId: Long
+        @RequestBody request: TodoUpdateRequest,
+        @Parameter(hidden = true) @ModelAttribute memberDetails : MemberDetails
     ): ResponseEntity<TodoResponse> {
-        // TODO: 나중에 memberId 제외하고 헤더에서 jwt를 통해서 값 받기
+
+        if (memberDetails.id == null) {
+            throw IllegalArgumentException("인증/인가 안됨")
+        }
+
         return try{
-            ResponseEntity.status(HttpStatus.OK).body(todoService.updateTodo(todoId, request, memberId))
+            ResponseEntity.status(HttpStatus.OK).body(todoService.updateTodo(todoId, request, memberDetails.id!!))
         } catch(e: IllegalStateException){
             ResponseEntity.status(HttpStatus.BAD_REQUEST).build()
         }
 
     }
 
+    @MemberPrincipal("USER")
     @PatchMapping("/{id}")
     fun updateTodoDone(
         @PathVariable(value = "id") todoId: Long,
-        @RequestParam(name = "memberId") memberId: Long
+        @Parameter(hidden = true) @ModelAttribute memberDetails : MemberDetails
     ): ResponseEntity<TodoResponse> {
-        // TODO: 나중에 memberId 제외하고 헤더에서 jwt를 통해서 값 받기
-        return ResponseEntity.status(HttpStatus.OK).body(todoService.updateTodoDone(todoId, memberId))
+
+        if (memberDetails.id == null) {
+            throw IllegalArgumentException("인증/인가 안됨")
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(todoService.updateTodoDone(todoId, memberDetails.id!!))
     }
 
+    @MemberPrincipal("USER")
     @DeleteMapping("/{id}")
     fun deleteTodo(
         @PathVariable(value = "id") todoId: Long,
-        @RequestParam(name = "memberId") memberId: Long
+        @Parameter(hidden = true) @ModelAttribute memberDetails : MemberDetails
     ): ResponseEntity<Unit> {
-        // TODO: 나중에 memberId 제외하고 헤더에서 jwt를 통해서 값 받기
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(todoService.deleteTodo(todoId, memberId))
+
+        if (memberDetails.id == null) {
+            throw IllegalArgumentException("인증/인가 안됨")
+        }
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(todoService.deleteTodo(todoId, memberDetails.id!!))
     }
 
 }
