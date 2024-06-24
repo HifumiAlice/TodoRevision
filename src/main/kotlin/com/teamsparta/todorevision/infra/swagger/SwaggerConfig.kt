cf8d5b3d22da96cc8cefaa592dfbc9457608a707
Alice <hifumialice@gmail.com>
2024-06-24 16:26:48 +0900

    authorization 헤더 생성

--- src/main/kotlin/com/teamsparta/todorevision/infra/swagger/SwaggerConfig.kt
+++ src/main/kotlin/com/teamsparta/todorevision/infra/swagger/SwaggerConfig.kt
@@ -1,10 +1,24 @@
 package com.teamsparta.todorevision.infra.swagger
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.security.SecurityScheme
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+
+
+@SecurityScheme(
+    type = SecuritySchemeType.HTTP,
+    `in` = SecuritySchemeIn.HEADER,
+    name = "Authorization",
+    description = "Auth Token Header",
+    bearerFormat = "JWT",
+    scheme = "Bearer"
+)
 
 @Configuration
 class SwaggerConfig {
@@ -18,6 +32,11 @@ class SwaggerConfig {
                     .title("Swagger API")
                     .description("나도 몰루?")
                     .version("v1.0.0")
+            )
+            .security(
+                listOf(
+                    SecurityRequirement().addList("Authorization")
+                )
             )
     }
 }
