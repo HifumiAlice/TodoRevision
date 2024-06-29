98d0fbf5c73a3991a3299758596ec8f893caf2f7
Alice <hifumialice@gmail.com>
2024-06-29 22:41:15 +0900

    TestCode를 위한 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,6 +52,14 @@ dependencies {
     implementation("com.querydsl:querydsl-jpa:$queryDslVersion:jakarta") // 추가!
     kapt("com.querydsl:querydsl-apt:$queryDslVersion:jakarta") // 추가!
 
+
+//    ----- TestCode
+    testImplementation("io.kotest:kotest-runner-junit5:5.5.5") // 추가 !!
+    testImplementation("io.kotest:kotest-assertions-core:5.5.5") // 추가 !!
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3") // 추가 !!
+    testImplementation("io.mockk:mockk:1.13.8") // 추가 !!
+    runtimeOnly("com.h2database:h2")
+
 }
 
 noArg {
@@ -72,6 +80,6 @@ kotlin {
     }
 }
 
-tasks.withType<Test> {
+tasks.withType<Test>().configureEach() {
     useJUnitPlatform()
 }
